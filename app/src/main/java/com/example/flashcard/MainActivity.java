e5d7f17d8323baaea1473a9ff5de341ad5a5cc0a
VALLEE <valvallee@live.fr>
2025-10-08 13:32:51 +0200

    Story 9

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.flashcard;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -24,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Log.d(TAG, "Hello Flashcard");
-    }
-}
+        //When the Quizz button is pressed, check the choosen difficulty setting and send it
+        Button quizzButton = findViewById(R.id.quizzButton);
+        quizzButton.setOnClickListener(view ->{
+                Intent intent = new Intent(this, QuizActivity.class);
+                RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup);
+                intent.putExtra("difficulty",difficultyRadioGroup.getCheckedRadioButtonId());
+                startActivity(intent);
+        });
+        //Takes the user to the about activity
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(view ->{
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        });
+}}
