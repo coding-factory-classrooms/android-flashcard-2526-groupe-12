b7c7581f26a86266526f3a6058fdd92a96087f41
Matisk123 <matis.kohn@edu.esiee-it.fr>
2025-10-08 11:02:47 +0200

    Clik_change_image

--- app/src/main/java/com/example/flashcard/MediaInteractActivity.java
+++ app/src/main/java/com/example/flashcard/MediaInteractActivity.java
@@ -1,6 +1,8 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class QuizActivity extends AppCompatActivity {
+public class MediaInteractActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_media_interact);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView UKFlag = (ImageView) findViewById(R.id.UKFlag);
+
+        UKFlag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UKFlag.setImageResource(R.drawable.flag_japon);
+            }
+        });
+
 
     }
 }
