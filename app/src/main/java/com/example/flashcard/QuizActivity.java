430ec6586842659f90b6d39461bacc38c99ea30c
MaelP12 <mael.philisot@edu.esiee-it.fr>
2025-10-08 09:53:25 +0200

    radio group

--- app/src/main/java/com/example/flashcard/QuizActivity.java
+++ app/src/main/java/com/example/flashcard/QuizActivity.java
@@ -1,15 +1,24 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class QuizActivity extends AppCompatActivity {
 
+
+    TextView resultView;
+    RadioGroup inputRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,19 @@ public class QuizActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resultView = findViewById(R.id.resultView);
+        inputRadioGroup = findViewById(R.id.inputRadioGroup);
+
+
+        inputRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                resultView.setText(radioButton.getText());
+            }
+        });
+
+
     }
 }
