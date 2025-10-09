c7076e153df7a91fc92d5115d726a084b02fbf4b
VALLEE <valvallee@live.fr>
2025-10-09 09:59:16 +0200

    Story 3 & 6

--- app/src/main/java/com/example/flashcard/QuizLoader.java
+++ app/src/main/java/com/example/flashcard/QuizLoader.java
@@ -1,6 +1,7 @@
 package com.example.flashcard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class QuizLoader {
 
@@ -13,20 +14,24 @@ public class QuizLoader {
         questions.add("Commencer un massage cardiaque");
         questions.add("Lui rouler une pelle");
         questions.add("Tu est dans une tempête d'émotion et c'est ok");
-
-        quizList.add(new Quiz(R.drawable.arret, questions, 1, 0));
-
-        questions = new ArrayList<>();
-        questions.add("Japon");
-        questions.add("Royaume-Uni");
-        questions.add("Espagne");
-        quizList.add(new Quiz(R.drawable.flag_uk, questions, 0, 0));
+        Collections.shuffle(questions);
+        quizList.add(new Quiz(R.drawable.arret, questions, questions.indexOf("Lui rouler une pelle"), 0));
 
         questions = new ArrayList<>();
         questions.add("Japon");
         questions.add("Royaume-Uni");
         questions.add("Espagne");
-        quizList.add(new Quiz(R.drawable.flag_england, questions, 1, 0));
+        Collections.shuffle(questions);
+        quizList.add(new Quiz(R.drawable.flag_uk, questions, questions.indexOf("Japon"), 0));
+
+        questions = new ArrayList<>();
+        questions.add("Japon");
+        questions.add("Royaume-Uni");
+        questions.add("Espagne");
+        Collections.shuffle(questions);
+        quizList.add(new Quiz(R.drawable.flag_england, questions, questions.indexOf("Royaume-Uni"), 0));
+
+        Collections.shuffle(quizList);
 
         return quizList;
     }
